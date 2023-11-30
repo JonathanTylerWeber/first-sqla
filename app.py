e0184ccc9b917a963b0525dff657a983a4404a0f
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-11-30 19:35:26 +0800

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = flask_debugtoolbardebug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route('/')
+def home_page():
+    """Shows home page"""
+    return render_template('home.html')  
